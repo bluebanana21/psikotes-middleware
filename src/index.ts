d82b1356103be3861ee1d8ca7f0af398255eba8a
bluebanana21 <m.affanfahrozi@protonmail.com>
2025-08-29 15:25:21 +0700

    chore: init routing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {type Request, type Response} from "express";
+import express, { type Request, type Response } from "express";
 import bookRoutes from "./routes/bookRoutes.ts";
 
 const app = express();
@@ -9,9 +9,9 @@ app.use(express.json());
 app.use("/api", bookRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome to Node.js + typescript API");
+  res.send("Welcome to Node.js + typescript API");
 });
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
+  console.log(`server is running on port ${PORT}`);
+});
--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,66 +1,69 @@
-import {Router, type Request, type Response} from "express";
+import { Router, type Request, type Response } from "express";
 
 const router = Router();
 
 interface Book {
-    id: number;
-    title: string;
-    author: string;
+  id: number;
+  title: string;
+  author: string;
 }
 
 const books: Book[] = [
-    {id:1, title:"1984", author:"George Orwell"},
-    {id:2, title:"The Road", author:"Cormac McCarthy"},
+  { id: 1, title: "1984", author: "George Orwell" },
+  { id: 2, title: "The Road", author: "Cormac McCarthy" },
 ];
 
 router.get("/books", (req: Request, res: Response) => {
-    res.json(books);
+  res.json(books);
 });
 
-router.get("/books/:id", (req: Request, res: Response)=>{
-    const bookId = parseInt(req.params.id!);
-    const book = books.find(b=>b.id === bookId);
-    if (book) {
-        res.json(book);
-    } else {
-        res.status(404).json({message: "Book not found!"});
+router.get("/books/:id", (req: Request, res: Response) => {
+  const bookId = parseInt(req.params.id!);
+  const book = books.find((b) => b.id === bookId);
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ message: "Book not found!" });
+  }
+});
+
+router.post("/books", (req: Request, res: Response) => {
+  const newBook: Book = {
+    id: books.length + 1,
+    title: req.body.title,
+    author: req.body.author,
+  };
+
+  books.push(newBook);
+  res.status(201).json(newBook);
+});
+
+router.put("/books/:id", (req: Request, res: Response) => {
+  const bookId = parseInt(req.params.id!);
+  const bookIndex = books.findIndex((b) => b.id === bookId);
+
+  if (bookIndex !== 1) {
+    books[bookIndex] = {
+      id: bookId,
+      title: req.body.title,
+      author: req.body.author,
     };
+    res.json(books[bookIndex]);
+  } else {
+    res.status(404).json({ message: "Book not found!" });
+  }
 });
 
-router.post("/books", (req: Request, res:Response)=>{
-    const newBook: Book = {
-        id: books.length+1,
-        title: req.body.title,
-        author: req.body.author,
-    };
+router.delete("/books/:id", (req: Request, res: Response) => {
+  const bookId = parseInt(req.params.id!);
+  const bookIndex = books.findIndex((b) => b.id === bookId);
 
-    books.push(newBook);
-    res.status(201).json(newBook);
-});
-
-router.put("/books/:id", (req: Request, res: Response)=>{
-    const bookId = parseInt(req.params.id!);
-    const bookIndex = books.findIndex(b => b.id === bookId);
-
-    if (bookIndex !== 1) {
-        books[bookIndex] = {id: bookId, title:req.body.title, author:req.body.author };
-        res.json(books[bookIndex]);
-
-    } else {
-        res.status(404).json({message:"Book not found!"});
-    }
-});
-
-router.delete("/books/:id", (req:Request, res:Response)=>{
-    const bookId = parseInt(req.params.id!);
-    const bookIndex = books.findIndex(b=>b.id === bookId);
-
-    if (bookIndex !== -1) {
-        books.splice(bookIndex, 1);
-        res.status(204).send();
-    } else {
-        res.status(404).json({message:"book not fund"});
-    }
+  if (bookIndex !== -1) {
+    books.splice(bookIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).json({ message: "book not fund" });
+  }
 });
 
 export default router;
